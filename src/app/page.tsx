d27191e5cedce52800b1ca4c9c98d80caf57f731
k-xo <alkassimk@gmail.com>
2024-02-29 06:43:08 +0100

    feat: passkey demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
 "use client";
-import { useGoogleLogin } from "@react-oauth/google";
+import { useEffect, useState } from "react";
 import { Passport } from "@0xpass/passport";
-import { useCallback, useEffect, useState } from "react";
-import OTPInput from "./components/OTPInput";
+import { WebauthnSigner } from "@0xpass/webauthn-signer";
 import axios from "axios";
 import { createPassportClient } from "@0xpass/passport-viem";
 import { mainnet } from "viem/chains";
 import { http, WalletClient } from "viem";
 import { enqueueSnackbar } from "notistack";
 import { JsonViewer } from "@textea/json-viewer";
+import { ENCLAVE_PUBLIC_KEY } from "./helpers";
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [otp, setOtp] = useState("");
-  const [session, setSession] = useState("");
+  const [username, setUsername] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+  const [authenticatedHeader, setAuthenticatedHeader] = useState({});
 
-  const [oauthLoading, setoauthLoading] = useState(false);
-  const [sendOtpLoading, setSendOtpLoading] = useState(false);
-  const [submitOtpLoading, setSubmitOtpLoading] = useState(false);
   const [signMessageLoading, setSignMessageLoading] = useState(false);
   const [signTxLoading, setSignTxLoading] = useState(false);
+  const [authenticateSetup, setAuthenticateSetup] = useState(false);
+  const [authenticating, setAuthenticating] = useState(false);
 
   const [address, setAddress] = useState<any>("");
 
@@ -37,6 +36,24 @@ export default function Home() {
     prepareTransactionTimeTaken: number;
   } | null>(null);
 
+  const [signer, setSigner] = useState<null | WebauthnSigner>(null);
+
+  useEffect(() => {
+    const signer = new WebauthnSigner({
+      rpId: window.location.hostname,
+      rpName: "0xPass",
+    });
+
+    setSigner(signer);
+  }, []);
+
+  const passport = new Passport({
+    scope_id: "768b0dfd-3284-4d9f-b611-4418c32b491e",
+    signer: signer!,
+    endpoint: "https://waffle.0xpass.io",
+    enclave_public_key: ENCLAVE_PUBLIC_KEY,
+  });
+
   useEffect(() => {
     async function fetchAddress() {
       const client: WalletClient = createWalletClient();
@@ -44,47 +61,107 @@ export default function Home() {
       setAddress(response);
     }
 
-    if (session) {
+    if (Object.keys(authenticatedHeader).length > 0) {
       fetchAddress();
     }
-  }, [session]);
-
-  const passport = new Passport();
+  }, [authenticatedHeader]);
 
   const alchemyUrl = process.env.NEXT_PUBLIC_ALCHEMY_URL;
-  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-  const googleClientSecret = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;
-
   const fallbackProvider = http(alchemyUrl);
 
-  const handleOTPChange = useCallback((otp: any) => {
-    setOtp(otp);
-  }, []);
+  const userInput = {
+    username: username,
+    userDisplayName: username,
+  };
 
-  const googleLogin = useGoogleLogin({
-    flow: "auth-code",
-    onSuccess: async (codeResponse) => {
-      setoauthLoading(true);
-      const token = await getAccessTokenFromCode(codeResponse.code);
+  async function register() {
+    if (username.trim().length === 0) {
+      enqueueSnackbar("Username cannot be empty", { variant: "error" });
+      return;
+    }
 
-      const startKeygen = performance.now();
-      const session = await passport.getSession({
-        scope_id: "1",
-        verifier_type: "google",
-        code: token.toString(),
-      });
-      const endKeygen = performance.now();
-      setKeygenTime(endKeygen - startKeygen);
+    // The `passport.register` method triggers a passkey modal flow and the time taken
+    // to complete the modal by the user is included in the keygen time. So we intercept
+    // the request and response to get the actual time taken specific to the keygen process.
 
-      setSession(JSON.stringify(session.result));
-      sessionStorage.setItem("session", JSON.stringify(session.result));
-      setoauthLoading(false);
-    },
-    onError: (errorResponse) => console.log(errorResponse),
-  });
+    let requestStartTime = 0;
+    const requestInterceptor = axios.interceptors.request.use((request) => {
+      if (
+        request.url === "https://waffle.0xpass.io" &&
+        request.data &&
+        request.data.method === "completeRegistration"
+      ) {
+        console.log("Completing registration request:", request);
+        requestStartTime = performance.now();
+      }
+      return request;
+    });
+
+    const responseInterceptor = axios.interceptors.response.use(
+      (response) => {
+        if (
+          response.config.url === "https://waffle.0xpass.io" &&
+          response.config.data
+        ) {
+          const requestData = JSON.parse(response.config.data);
+          if (requestData.method === "completeRegistration") {
+            console.log("Completing registration response:", response);
+            const timeTaken = performance.now() - requestStartTime;
+            setKeygenTime(timeTaken);
+          }
+        }
+        return response;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+
+    try {
+      await passport.setupEncryption();
+
+      const res = await passport.register(userInput);
+      console.log(res);
+      //@ts-ignore
+      if (res.result.account_id) {
+        setAuthenticating(true);
+        await authenticate();
+        setAuthenticating(false);
+      }
+    } catch (error) {
+      console.error("Error registering:", error);
+      enqueueSnackbar(
+        "Error registering, make sure you are registering a unique username",
+        { variant: "error" }
+      );
+    } finally {
+      axios.interceptors.request.eject(requestInterceptor);
+      axios.interceptors.response.eject(responseInterceptor);
+    }
+  }
+
+  async function authenticate() {
+    try {
+      await passport.setupEncryption();
+      const [authenticatedHeader, wallet] = await passport.authenticate(
+        userInput
+      );
+      setAuthenticatedHeader(authenticatedHeader);
+      console.log(wallet);
+      setAddress(wallet);
+      setAuthenticated(true);
+    } catch (error) {
+      console.error("Error registering:", error);
+    }
+  }
 
   function createWalletClient() {
-    return createPassportClient(session, fallbackProvider, mainnet);
+    return createPassportClient(
+      authenticatedHeader,
+      fallbackProvider,
+      mainnet,
+      "https://waffle.0xpass.io"
+    );
   }
 
   async function signMessage(message: string) {
@@ -141,40 +218,13 @@ export default function Home() {
     }
   }
 
-  async function getAccessTokenFromCode(code: any): Promise<String> {
-    const tokenUrl = "https://oauth2.googleapis.com/token";
-
-    const data = {
-      code: code,
-      client_id: googleClientId,
-      client_secret: googleClientSecret,
-      redirect_uri: window.location.origin,
-      grant_type: "authorization_code",
-    };
-
-    try {
-      const response = await axios.post(tokenUrl, data, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      });
-
-      // Extract the access token (and optionally the refresh token)
-      return response.data.access_token;
-    } catch (error) {
-      console.error("Error fetching access token");
-      throw error;
-    }
-  }
-
   return (
     <main>
       <div className="p-12">
         <h1 className="text-6xl font-bold">Passport Demo</h1>
         <p className="max-w-[40ch] leading-7 mt-8">
-          Effortlessly set up your self-custody wallet with a few simple taps,
-          your favorite OAuth Providers or email OTP, all while maintaining
-          complete self-custody through an MPC network. Say goodbye to
+          Effortlessly set up your self-custody wallet with a few simple taps /
+          clicks, by creating a passkey, with a unique username say goodbye to
           passwords, seed phrases, and private keys.{" "}
           <a
             className="italic leading-8 underline underline-offset-4"
@@ -186,8 +236,8 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="flex space-y-5 flex-col items-center max-w-xl mx-auto">
-        {session ? (
+      <div className="flex space-y-5 flex-col items-center max-w-xl mx-auto mt-16">
+        {authenticated ? (
           <div className="p-6 w-full">
             <p>Connected account: {address} </p>
             {keygenTime && (
@@ -264,79 +314,39 @@ export default function Home() {
               >
                 {signTxLoading ? "Loading..." : "Sign Transaction"}
               </button>
-
-              <a href="/lambda">
-                <div className="w-full border border-1 rounded p-2 mt-2 hover:cursor-pointer text-center">
-                  Try Passport Lambda
-                </div>
-              </a>
             </div>
           </div>
         ) : (
-          <div className="flex flex-col items-stretch space-y-8">
-            <button
-              onClick={googleLogin}
-              className="p-2 border border-1 border-gray-600 rounded"
-            >
-              {oauthLoading ? "Loading..." : " Login with Google ✨"}
-            </button>
-            <div className="flex space-x-2">
+          <div className="flex flex-col items-stretch space-y-8 w-full">
+            <div className="flex flex-col items-center space-y-5 w-full">
               <input
                 type="text"
-                value={email}
+                placeholder="Enter a unique username"
+                value={username}
                 onChange={(e) => {
-                  setEmail(e.target.value);
+                  setUsername(e.target.value);
                 }}
-                className="flex-grow border border-1 bg-[#161618] border-gray-600 focus:outline-black rounded p-2"
+                className="w-4/6 border border-1 bg-[#161618] border-gray-600 focus:outline-black rounded p-3 text-center"
               />
               <button
-                className="flex-grow border border-1 rounded p-2"
-                onClick={async () => {
-                  setSendOtpLoading(true);
-                  await passport.sendOtp({
-                    scope_id: "1",
-                    channel_type: "email",
-                    destination: email,
-                  });
-                  setSendOtpLoading(false);
-
-                  enqueueSnackbar("", {
-                    content: () => (
-                      <div className="p-4 bg-[#20242A] text-white rounded-lg text-sm">
-                        OTP Sent successfully <br />
-                      </div>
-                    ),
-                  });
-                }}
+                className="w-4/6 border border-1 rounded p-3"
+                onClick={authenticateSetup ? authenticate : register}
               >
-                {sendOtpLoading ? "Loading..." : "Send OTP"}
+                {authenticateSetup
+                  ? "Authenticate"
+                  : authenticating
+                  ? "Authenticating..."
+                  : "Register"}
               </button>
-            </div>
-            <div>
-              <OTPInput length={6} onChange={handleOTPChange} />
-              <button
-                className="w-full border border-1 rounded p-2 mt-10"
-                onClick={async () => {
-                  setSubmitOtpLoading(true);
-                  const keygenTimeStart = performance.now();
-                  const response = await passport.getSession({
-                    scope_id: "1",
-                    verifier_type: "email",
-                    code: otp,
-                  });
-                  const keygenTimeEnd = performance.now();
 
-                  setSession(JSON.stringify(response.result));
-                  sessionStorage.setItem(
-                    "session",
-                    JSON.stringify(response.result)
-                  );
-                  setKeygenTime(keygenTimeEnd - keygenTimeStart);
-                  setSubmitOtpLoading(false);
-                }}
+              <span
+                onClick={() => setAuthenticateSetup(!authenticateSetup)}
+                className="cursor-pointer"
               >
-                {submitOtpLoading ? "Loading..." : "Submit OTP"}
-              </button>
+                {authenticateSetup
+                  ? "Register a Passkey?"
+                  : "Already have a passkey?"}
+              </span>
             </div>
           </div>
         )}
