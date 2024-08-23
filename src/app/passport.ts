3c1d066177248c6cb9eda948948d1f47136a99bf
k-xo <alkassimk@gmail.com>
2024-08-23 20:14:11 +0100

    chore: mainnet

--- src/app/passport.ts
+++ src/app/passport.ts
@@ -5,7 +5,7 @@ const keySigner = new KeySigner(process.env.PRIVATE_KEY!, true);
 
 export const passport = new Passport({
   network:
-    process.env.NODE_ENV === "production" ? Network.TESTNET : Network.LOCAL,
+    process.env.NODE_ENV === "production" ? Network.MAINNET : Network.LOCAL,
   scopeId: process.env.NEXT_PUBLIC_SCOPE_ID!,
   signer: keySigner,
 });
